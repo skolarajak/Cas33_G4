28d55bb713ed52ca7cee2e5931ffcc8685040ea5
Skola Rajak QA <skolarajak.qa@gmail.com>
2019-06-02 16:44:36 +0200

    Add project files.

---
+++ Cas33_G4/FileClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Cas33_G4
+{
+    class FileClass
+    {
+        static public void Log(string FileName, string LogMessage)
+        {
+            // Write a log file to the specified location, if file exists, append text to the end
+            using (StreamWriter file = new StreamWriter(FileName, true))
+            {
+                file.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss K"));
+                file.WriteLine(LogMessage);
+                file.WriteLine("**********");
+                file.WriteLine();
+            }
+        }
+    }
+}
---
+++ Cas33_G4/SeleniumTests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Cas33_G4
+{
+    class SeleniumTests
+    {
+        // Declare a variable to hold our webdriver reference
+        IWebDriver driver;
+
+        [SetUp]
+        // This method is called automatically every time a test is run
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void TestTableLondonOfficeAge()
+        {
+            GoToURL("https://www.seleniumeasy.com/test/table-sort-search-demo.html", 1000, 3000);
+            FindElement(By.XPath("//input[@type='search']")).SendKeys("London");
+            Wait(5000);
+            IList<IWebElement> rows = FindElement(By.Id("example")).FindElement(By.TagName("tbody")).FindElements(By.TagName("tr"));
+            string[] ages = { "47", "41", "43", "30", "66", "53", "19" };
+            int counter = 0;
+            int failed = 0;
+            foreach(IWebElement row in rows)
+            {
+                IList<IWebElement> columns = row.FindElements(By.TagName("td"));
+                IWebElement age = columns[3];
+                if (age.Text != ages[counter])
+                {
+                    failed++;
+                }
+                counter++;
+            }
+
+            if (failed > 0)
+            {
+                Assert.Fail("There is a discrepancy in ages retrieved.");
+            } else
+            {
+                Assert.Pass("OK");
+            }
+        }
+
+        [TearDown]
+        // This method is called automatically every time a test has finished its run
+        public void TearDown()
+        {
+            // Close our webdriver and release memory
+            driver.Close();
+        }
+
+        static private void Wait(int ms)
+        {
+            System.Threading.Thread.Sleep(ms);
+        }
+
+        private void GoToURL(string url, int msBefore = 1000, int msAfter = 2000)
+        {
+            Wait(msBefore);
+            driver.Navigate().GoToUrl(url);
+            Wait(msAfter);
+        }
+
+        public IWebElement FindElement(By selector)
+        {
+            // Declare a variable to hold our found element
+            IWebElement elReturn = null;
+
+            try
+            {
+                elReturn = driver.FindElement(selector);
+            } catch (NoSuchElementException)
+            {
+                string File = "C:\\Kurs\\Cas33_G4.log";
+                string Message = $"Element \"{selector.ToString()}\" is not found.";
+                FileClass.Log(File, Message);
+
+            } catch (Exception e)
+            {
+                throw e;
+            }
+
+            return elReturn;
+        }
+    }
+}
